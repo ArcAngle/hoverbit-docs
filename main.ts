be720c71c8db5d6f57340623baaa00b42fd6645a
ArcAngle <eddy@3os.mobi>
2022-05-25 12:52:17 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let Roll = 0
 basic.forever(function () {
-	
+    let Throttle = 0
+    let Arm = 0
+    Roll = input.rotation(Rotation.Roll)
+    basic.clearScreen()
+    if (Arm) {
+        led.plot(0, 0)
+    }
+    led.plot(0, 4 - Throttle / 25)
 })
